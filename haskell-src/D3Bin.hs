820025af58836214d20e6177b9e03ce358ce9fb7
Aurorans Solis <primalucegd@gmail.com>
2023-12-07 01:22:35 -0700

    d3p1. It shouldn't work but it does.

---
+++ haskell-src/D3Bin.hs
@@ -0,0 +1,4 @@
+import D3Lib
+import Misc
+
+main = Misc.aocMain "input/d3.txt" D3Lib.part1 D3Lib.part2
--- haskell-src/D3Lib.hs
+++ haskell-src/D3Lib.hs
@@ -3,72 +3,128 @@ module D3Lib (part1, part2) where
 import qualified Data.Char
 import qualified Misc
 
-data CellVal = Empty | Symbol | Digit Int Int
+data CellVal = Empty | Symbol | Digit Int deriving (Show, Eq)
 readCellVal :: Char -> CellVal
 readCellVal char
-    | Data.Char.isDigit char = Data.Char.ord char - Misc.zeroDigit
+    | Data.Char.isDigit char = Digit $ Data.Char.ord char - Misc.zeroDigit
     | isSymbol char = Symbol
     | otherwise = Empty
   where
-    isSymbol c = 
+    isSymbol c = c `elem` ['#', '%', '&', '*', '+', '-', '/', '=', '@', '$']
+
+toDigit :: CellVal -> Int
+toDigit (Digit val) = val
+toDigit _ = 0
+
+isDigit :: CellVal -> Bool
+isDigit (Digit _) = True
+isDigit _ = False
 
 part1 :: String -> Int
-part1 _ = 0
-
-type Coord = (Int, Int)
-
-part1Rec :: String -> Int
-part1Rec
+part1 str = processRowStart $ makeProcess $ part1ReadGrid str
 
 part1ReadGrid :: String -> [[CellVal]]
-part1ReadGrid grid = cons(case breakGrid grid of
-    (l, r) -> (l, case r of
-        [] -> []
-        _:r' -> breakGrid r'))
+part1ReadGrid grid =
+    cons
+        ( case breakGrid grid of
+            (line, rest) ->
+                ( line
+                , case rest of
+                    [] -> []
+                    _ : rest' -> part1ReadGrid rest'
+                )
+        )
+  where
+    cons ~(a, b) = a : b
 
-breakGrid _ [] = ([], [])
+breakGrid :: [Char] -> ([CellVal], String)
+breakGrid [] = ([], [])
 breakGrid all@(head : tail)
     | head == '\n' = ([], all)
-    | otherwise = let (nextHead, nextTail) = breakGrid tail in (head:nextHead, nextTail)
+    | otherwise = let (nextHead, nextTail) = breakGrid tail in (headVal : nextHead, nextTail)
   where
-    headVal = 
-{-
-break _ xs@[] =  (xs, xs)
-break p xs@(x:xs')
-           | p x        =  ([],xs)
-           | otherwise  =  let (ys,zs) = break p xs' in (x:ys,zs)
+    headVal = readCellVal head
 
-break (== 1) [2, 1, 0]
+data ProcessGrid = ProcessGrid
+    { r0 :: [CellVal]
+    , r1 :: [CellVal]
+    , r2 :: [CellVal]
+    , rest :: [[CellVal]]
+    }
 
-break p all@(2:[1, 0]) -> let (ys, zs) = break p [1, 0] in 
+makeProcess :: [[CellVal]] -> ProcessGrid
+makeProcess all@(r1 : r2 : rest) = ProcessGrid [] r1 r2 all
 
-lines s = cons (case break (== '\n') s of
-    (l, s') -> (l, case s' of
-        [] -> []
-        _:s'' -> lines s''))
+processRowStart :: ProcessGrid -> Int
+-- Start case - no first line
+processRowStart (ProcessGrid [] (r10 : r1Rest) (r20 : r2Rest) rest) = case r10 of
+    Digit val -> processRow (ProcessGrid [] r1Rest r2Rest rest) (r20 == Symbol) val
+    Symbol -> processRow (ProcessGrid [] r1Rest r2Rest rest) True 0
+    Empty -> processRow (ProcessGrid [] r1Rest r2Rest rest) (r20 == Symbol) 0
+-- End case - no last line
+processRowStart (ProcessGrid (r00 : r0Rest) (r10 : r1Rest) [] rest) = case r10 of
+    Digit val -> processRow (ProcessGrid r0Rest r1Rest [] rest) (r00 == Symbol) val
+    Symbol -> processRow (ProcessGrid r0Rest r1Rest [] rest) True 0
+    Empty -> processRow (ProcessGrid r0Rest r1Rest [] rest) (r00 == Symbol) 0
+-- General case
+processRowStart (ProcessGrid (r00 : r0Rest) (r10 : r1Rest) (r20 : r2Rest) rest) = case r10 of
+    Digit val -> processRow (ProcessGrid r0Rest r1Rest r2Rest rest) (r00 == Symbol || r20 == Symbol) val
+    Symbol -> processRow (ProcessGrid r0Rest r1Rest r2Rest rest) True 0
+    Empty -> processRow (ProcessGrid r0Rest r1Rest r2Rest rest) (r00 == Symbol || r20 == Symbol) 0
+
+processRow :: ProcessGrid -> Bool -> Int -> Int
+-- First row, final value
+processRow (ProcessGrid [] [r1Head] [r2Head] (newR0 : rest@(newR1 : newR2 : _))) hasSymbol prev = case r1Head of
+    Digit val -> digitFinal val + processRowStart (ProcessGrid newR0 newR1 newR2 rest)
+    Symbol -> prev + processRowStart (ProcessGrid newR0 newR1 newR2 rest)
+    Empty -> emptyFinal + processRowStart (ProcessGrid newR0 newR1 newR2 rest)
   where
-    cons ~(h, t) =  h : t
--}
-
+    newHasSymbol = hasSymbol || r2Head == Symbol
+    digitFinal v = if newHasSymbol then prev * 10 + v else 0
+    emptyFinal = if r2Head == Symbol then prev else 0
+-- First row, general value
+processRow (ProcessGrid [] (r1Head : r1Rest) (r2Head : r2Rest) rest) hasSymbol prev = case r1Head of
+    Digit val -> processRow (ProcessGrid [] r1Rest r2Rest rest) newHasSymbol (prev * 10 + val)
+    Symbol -> prev + processRow (ProcessGrid [] r1Rest r2Rest rest) True 0
+    Empty -> finalValue + processRow (ProcessGrid [] r1Rest r2Rest rest) (r2Head == Symbol) 0
+  where
+    newHasSymbol = hasSymbol || r2Head == Symbol
+    finalValue = if newHasSymbol then prev else 0
+-- Final row, final value
+processRow (ProcessGrid [r0Head] [r1Head] [] rest) hasSymbol prev = case r1Head of
+    Digit val -> digitFinal val
+    Symbol -> prev
+    Empty -> emptyFinal
+  where
+    newHasSymbol = hasSymbol || r0Head == Symbol
+    digitFinal v = if newHasSymbol then prev * 10 + v else 0
+    emptyFinal = if newHasSymbol then prev else 0
+-- Final row, general value
+processRow (ProcessGrid (r0Head : r0Rest) (r1Head : r1Rest) [] rest) hasSymbol prev = case r1Head of
+    Digit val -> processRow (ProcessGrid r0Rest r1Rest [] rest) newHasSymbol (prev * 10 + val)
+    Symbol -> prev + processRow (ProcessGrid r0Rest r1Rest [] rest) True 0
+    Empty -> finalValue + processRow (ProcessGrid r0Rest r1Rest [] rest) (r0Head == Symbol) 0
+  where
+    newHasSymbol = hasSymbol || r0Head == Symbol
+    finalValue = if newHasSymbol then prev else 0
+-- General row, final value
+processRow (ProcessGrid [r0Head] [r1Head] [r2Head] (newR0 : rest@(newR1 : newR2 : _))) hasSymbol prev = case r1Head of
+    Digit val -> digitFinal val + processRowStart (ProcessGrid newR0 newR1 newR2 rest)
+    Symbol -> prev + processRowStart (ProcessGrid newR0 newR1 newR2 rest)
+    Empty -> emptyFinal + processRowStart (ProcessGrid newR0 newR1 newR2 rest)
+  where
+    newHasSymbol = hasSymbol || r0Head == Symbol || r2Head == Symbol
+    digitFinal v = if newHasSymbol then prev * 10 + v else 0
+    emptyFinal = if newHasSymbol then prev else 0
+-- General row, general value
+processRow (ProcessGrid (r0Head : r0Rest) (r1Head : r1Rest) (r2Head : r2Rest) rest) hasSymbol prev = case r1Head of
+    Digit val -> processRow (ProcessGrid r0Rest r1Rest r2Rest rest) newHasSymbol (prev * 10 + val)
+    Symbol -> prev + processRow (ProcessGrid r0Rest r1Rest r2Rest rest) True 0
+    Empty -> finalValue + processRow (ProcessGrid r0Rest r1Rest r2Rest rest) vertSymbol 0
+  where
+    vertSymbol = r0Head == Symbol || r2Head == Symbol
+    newHasSymbol = hasSymbol || vertSymbol
+    finalValue = if newHasSymbol then prev else 0
 
 part2 :: String -> Int
 part2 _ = 0
-
-data Tree a = Empty | Node a (Tree a) (Tree a)
-
-singleton :: a -> Tree a
-singleton x = Node x Empty Empty
-
-treeInsert :: (Ord a) => Tree a -> a -> Tree a
-treeInsert Empty x = singleton x
-treeInsert (Node a l r) b
-    | a == b = Node a l r
-    | a < b = Node a (treeInsert l b) r
-    | a > b = Node a l (treeInsert r b)
-
-treeHas :: (Ord a) => Tree a -> a -> bool
-treeHas Nothing a = False
-treeHas (Node a l r) b
-    | a == b = True
-    | a < b = treeHas l b
-    | a > b = treeHas r b
